e45ba0af0904325bce427e74270868ae85b9869c
Scoiatael85 <sdavis1985@gmail.com>
2017-02-27 03:18:16 -0500

    added a bit of meat to the about and example pages

--- app/components/About.jsx
+++ app/components/About.jsx
@@ -1,6 +1,13 @@
 var React = require('react');
 
 
-var About = (props) => <h3>About Component</h3>;
+var About = (props) => {
+	return (
+		<div>
+			<h3>About Component</h3>
+			<p>This is what it's all about</p>
+		</div>
+	)
+}
 
 module.exports = About;
--- app/components/Examples.jsx
+++ app/components/Examples.jsx
@@ -1,5 +1,16 @@
 var React = require('react');
 
 
-var Examples = (props) => <h3>Examples Component</h3>;
+var Examples = (props) => {
+	return (
+		<div>
+			<h3>Examples Component</h3>
+			<p>Here are a few examples:</p>
+			<li>First Example</li>
+			<li>2nd Example</li>
+			<li>Example C</li>
+			<li>Fifth Example</li>
+		</div>
+	)
+}
 module.exports = Examples;
