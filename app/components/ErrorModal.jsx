eed8e29cdf618b48cc6fd9df2e6c32621c3a796c
Scoiatael85 <sdavis1985@gmail.com>
2017-02-27 23:12:46 -0500

    correct error message for errorModal

--- app/components/ErrorModal.jsx
+++ app/components/ErrorModal.jsx
@@ -6,15 +6,20 @@ var ErrorModal = React.createClass({
 			title: 'Error'
 		};
 	},
+	propTypes: {
+		title: React.PropTypes.string,
+		message: React.PropTypes.string.isRequired
+	},
 	componentDidMount: function () {
 		var modal = new Foundation.Reveal($('#error-modal'));
 		modal.open();
 	},
 	render: function () {
+		var {title, message} = this.props;
 		return (
 			<div id="error-modal" className="reveal tiny text-center" data-reveal="">
-				<h4>Error Title</h4>
-				<p>temp Error Message</p>
+				<h4>{title}</h4>
+				<p>{message}</p>
 				<p>
 					<button className="hollow button" data-close="">
 						Okay
